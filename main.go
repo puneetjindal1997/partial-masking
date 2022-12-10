a840081fba54934ab358aeb543c048d7d5f51ae0
puneetjindal1997 <puneetjindal1997@gmail.com>
2022-12-10 21:02:22 +0530

    array case handled

--- main.go
+++ main.go
@@ -6,18 +6,29 @@ import (
 	"strings"
 )
 
-// "address": [{
-// 	"line_1": "dhaskjdask",
-// 	"line_2": "djaskdjask",
-// 	"city": "Bangalore"
-// }],
-
-// output now
-// {"email":"t*s*@*m*i*.*o*","message":"G*t*P*o*i*e*D*t*i*","name":"T*s*"}
+// expected output now
+// {"address":[{"city":"C*t*","line_1":"d*a*k*d*s*","line_2":"d*a*k*j*s*"},{"city":"C*t* *e*t","line_1":"a*d*e*s*1","line_2":"a*d*e*s*2"}],"address2":[{"city":"P*n*a*","line_1":"a*d*e*s* *h*s* *","line_2":"a*d*e*s* *o*e*a*d*e*s"},{"city":"c*d*n* *i*y","line_1":"d*m* *d*r*s*","line_2":"t*s*i*g*a*d*e*s"}],"email":"t*s*@*m*i*.*o*","message":"G*t*P*o*i*e*D*t*i*","name":"T*s*"}
 var data = `{
 	"name": "Test",
 	"email": "test@gmail.com",
-	
+	"address": [{
+		"line_1": "dhaskjdask",
+		"line_2": "djaskdjask",
+		"city": "City"
+	},{
+		"line_1": "address 1",
+		"line_2": "address 2",
+		"city": "City Test"
+	}],
+	"address2": [{
+		"line_1": "address2 phase 7",
+		"line_2": "address2 some address",
+		"city": "Punjab"
+	},{
+		"line_1": "demo address",
+		"line_2": "testing address",
+		"city": "coding city"
+	}],	
 	"message": "Got Profile Detail"
 }
 `
@@ -34,16 +45,43 @@ func main() {
 
 func partialMasking(unmarshalVar map[string]interface{}) []byte {
 	responseJson := map[string]interface{}{}
+	nestedJson := []map[string]interface{}{}
+	maskedString := ""
+	for k, v := range unmarshalVar {
+		switch v.(type) {
+		case string:
+			// fmt.Println(v, k)
+			arrayStr := strings.Split(v.(string), "")
+			maskedString = strMask(arrayStr)
+			responseJson[k] = maskedString
+			// fmt.Println(maskedString)
+
+		case []interface{}:
+			for _, va := range v.([]interface{}) {
+				resp := partialMaskingArray(va.(map[string]interface{}))
+				nestedJson = append(nestedJson, resp)
+				responseJson[k] = nestedJson
+			}
+			nestedJson = nil
+		}
+		bytest, _ = json.Marshal(responseJson)
+		// fmt.Println(string(bytest))
+	}
+	return bytest
+}
+
+func partialMaskingArray(unmarshalVar map[string]interface{}) map[string]interface{} {
+	responseJson := map[string]interface{}{}
+	maskedString := ""
 	for k, v := range unmarshalVar {
 		switch v.(type) {
 		case string:
 			arrayStr := strings.Split(v.(string), "")
-			result := strMask(arrayStr)
-			responseJson[k] = result
-			bytest, _ = json.Marshal(responseJson)
+			maskedString = strMask(arrayStr)
 		}
+		responseJson[k] = maskedString
 	}
-	return bytest
+	return responseJson
 }
 
 func strMask(s []string) string {
